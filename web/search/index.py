30e1c97782cd886e1f794cf1ac4472a90ed23171
shiro <yingjieho@hotmail.com>
2023-01-09 20:40:29 +0800

    adding bare search and index

---
+++ web/search/index.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+class IndexObj:
+    '''
+    idea
+    a cosine similarity "covariance" matrix
+    search by getting largest N documents
+
+    using pytorch's embedding with arbitrary size
+    to try out cosine similarity?
+
+    how to do validation?
+    '''
+    def __init__(self):
+        pass
+
+    def get_top_N_items(self):
+        pass
+
+    def index(self):
+        pass
+
+class Index:
+    pass
--- web/search/search.py
+++ web/search/search.py
@@ -1,10 +1,25 @@
+import os
+
+from .index import IndexObj
+
+
 class Search:
     '''
     TODO
     implement cosine similarity search?
     '''
-    def __init__(self):
-        pass
+    def __init__(self, absdir):
+        self.indexobj:IndexObj = IndexObj(absdir).index()
 
-    def index(self):
-        pass
+    def update_index(self):
+        self.indexobj.index()
+
+    def find_similar(self):
+        return self.indexobj.get_top_N_items()
+
+class SearchServer:
+    '''
+    motive
+    to run reindex outside of flask instance
+    '''
+    pass
