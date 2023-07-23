018c990092f3148c9d16d20a12e02357c05bca03
mushcatshiro <yingjieho@hotmail.com>
2023-07-24 07:09:43 +0800

    main views

--- web/blog/app/view.py
+++ web/blog/app/view.py
@@ -7,6 +7,18 @@ main = Blueprint("main", __name__)
 def index():
     return render_template("main.html")
 
+@main.route("/about", methods=["GET"])
+def about():
+    return render_template("about.html")
+
+@main.route("/blog/<title:str>", methods=["GET"])
+def blog(title):
+    return render_template("blog.html")
+
+@main.route("/notes/<title:str>", methods=["GET"])
+def notes(title):
+    return render_template("notes.html")
+
 @main.route("/admin")
 def admin():
     pass
