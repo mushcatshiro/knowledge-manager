a7612eac42d6e3e0311bdf0fe5c93b8ab2b7f30a
mushcatshiro <yingjieho@hotmail.com>
2023-08-05 18:47:24 +0800

    healthcheck

--- web/blog/api/endpoint.py
+++ web/blog/api/endpoint.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, jsonify, request, current_app, url_for
 import datetime as dt
 import os
+import time
 
 # from blog.models import Bookmark
 from blog.auth import verify_token
 from utils.database import PostgresConx
+from utils.healthcheck import server_healthcheck
 
 api = Blueprint("api", __name__)
 
@@ -18,3 +20,25 @@ def bookmark():
         return jsonify({"status": "success", "payload": payload})
     else:
         raise Exception("Invalid token")
+
+@api.route("/healthcheck", methods=["GET"])
+def healthcheck():
+    """
+    - Add database healthcheck
+    - Add redis healthcheck
+    - Add celery healthcheck
+    """
+    payload = {
+        "timestamp": dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "status": None,
+        "message": None,
+    }
+    try:
+        health = server_healthcheck()
+    except Exception as e:
+        payload["status"] = "error"
+        payload["message"] = str(e)
+    else:
+        payload["status"] = "success"
+        payload.update(health)
+    return jsonify(payload)
