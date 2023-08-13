3dd4d05879d8f84824d8110c18efa40e4bc0d58e
mushcatshiro <yingjieho@hotmail.com>
2023-08-13 22:44:23 +0800

    init draft

--- web/blog/app/view.py
+++ web/blog/app/view.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, current_app, request, send_from_directory
+from flask import Blueprint, render_template, current_app, request, send_from_directory, redirect, url_for
 import os
 import markdown
 
+from blog.auth import verify_token
 from blog.core import process_request
 
 main = Blueprint("main", __name__)
@@ -40,9 +41,11 @@ def blog():
     """
     blog_path = current_app.config["BLOG_PATH"]
     blog_list = os.listdir(blog_path)
+    output = []
     for i in range(len(blog_list)):
-        blog_list[i] = blog_list[i].replace(".md", "")
-    return render_template("blog.html", blog_list=blog_list, blog=True)
+        if blog_list[i].endswith(".md"):
+            output.append(blog_list[i].replace(".md", ""))
+    return render_template("blog.html", blog_list=output, blog=True)
 
 @main.route("/blog/<string:title>", methods=["GET"])
 def blog_with_title(title):
@@ -55,17 +58,41 @@ def blog_with_title(title):
             content = md.convert(content)
     return render_template("blog.html", content=content, blog=True)
 
-@main.route("/notes", methods=["GET"])
-def notes():
-    return render_template("notes.html", notes_list=[], notes=True)
-
-@main.route("/notes/<string:title>", methods=["GET"])
-def notes_with_title(title):
-    return render_template("notes.html", content="no content yet", notes=True)
-
 @main.route("/admin")
 def admin():
     pass
+
+@main.route("/draft", methods=["GET", "POST"])
+def draft():
+    content = ""
+    title = ""
+    if request.method == "POST":
+        content = request.form["content"]
+        title = request.form["title"]
+    return render_template("draft.html", content=content, title=title, draft=True)
+
+@main.route("/save", methods=["POST"])
+def save():
+    if verify_token(request.form["token"]):
+        blog_root = current_app.config["BLOG_PATH"]
+        blog_title = request.form["title"]
+        with open(os.path.join(blog_root, f"{blog_title}.md"), "w", encoding="utf-8") as f:
+            f.write(request.form["content"])
+        return redirect(url_for("main.blog"))
+    else:
+        raise Exception("Invalid token")
+
+@main.route("/preview", methods=["POST"])
+def preview():
+    content = request.form["content"]
+    title = request.form["title"]
+    converted_content = md.convert(content)
+    return render_template(
+        "preview.html",
+        content=content, 
+        title=title,
+        converted_content=converted_content
+    )
 
 @main.route("/favicon.ico")
 def favicon():
