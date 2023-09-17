ee99f6b308551bd0cb53424fb6c51808b8f936eb
yingjieho <yingjieho@hotmail.com>
2023-09-18 03:45:05 +0800

    test w/ mock and formatting

--- web/test/test_app/test_view.py
+++ web/test/test_app/test_view.py
@@ -1,8 +1,11 @@
+from blog.core.crud import CRUDBase
+
+
 def test_main_route_blog(test_app):
     client = test_app.test_client()
     response = client.get("/blog")
     assert response.status_code == 200
-    assert b"<a class=\"nav-link active\" href=\"/blog\">Blog</a>" in response.data
+    assert b'<a class="nav-link active" href="/blog">Blog</a>' in response.data
 
 
 def test_main_route_blog_dne(test_app):
@@ -12,8 +15,38 @@ def test_main_route_blog_dne(test_app):
     assert b"Blog does-not-exist not found!" in response.data
 
 
-def test_main_route_bookmarklet_list(test_app):
+def test_main_route_bookmarklet_list(test_app, monkeypatch):
+    def mock_query(*args, **kwargs):
+        return MockCrudBase().execute()
+
+    monkeypatch.setattr(CRUDBase, "execute", mock_query)
     client = test_app.test_client()
     response = client.get("/bookmarklet-list")
     assert response.status_code == 200
-    assert b"<a class=\"nav-link\" href=\"/bookmarklet-list\">Bookmarklet List</a>" in response.data
+    assert (
+        b'<a class="nav-link active" href="/bookmarklet-list">Reading List</a>'
+        in response.data
+    )
+
+
+class MockCrudBase:
+    def execute(self):
+        return [
+            MockBaseModel(),
+        ]
+
+
+class MockBaseModel:
+    def __init__(self):
+        self.id = 1
+        self.title = "test"
+        self.url = "http://test.com"
+        self.timestamp = "2020-01-01 00:00:00"
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "url": self.url,
+            "timestamp": self.timestamp,
+        }
