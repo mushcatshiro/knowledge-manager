49840dc60f2b11f03329b94d08c444d05b2d3472
mushcatshiro <yingjieho@hotmail.com>
2024-08-02 08:20:00 +0800

    organizing envvar

--- web/app.py
+++ web/app.py
@@ -1,5 +1,9 @@
 import os
 
 from blog import create_app
+from blog.utils.envvars import set_env_var
+
+
+set_env_var(".env.test")
 
 app = create_app(os.environ.get("FLASK_MODE") or "default")
--- web/cli.py
+++ web/cli.py
@@ -1,15 +1,46 @@
 import click
 
-
 from blog.utils.envvars import set_env_var
 
 
+"""
+TODO
+implement `migrate` and `revision` commands for alembic
+test
+"""
+
+
 @click.command()
-@click.option("--fname", default=".env", help="Name of the environment file.")
-def init(fname: str) -> None:
+@click.option("--env", default=".env", help="Name of the environment file.")
+def test(env: str) -> None:
     """Initialize the environment variables."""
-    set_env_var(fname)
+    set_env_var(env)
+
+    import pytest
+    import sys
+
+    print("Running tests...")
+    sys.exit(pytest.main(["-qq"]))
+
+
+@click.command()
+@click.option("--env", default=".env", help="Name of the environment file.")
+@click.option(
+    "--alembic-cfg",
+    default="alembic.ini",
+    help="Name of the alembic configuration file.",
+)
+def deploy(env: str, alembic_cfg) -> None:
+    """Initialize the environment variables."""
+    set_env_var(env)
+
+    from alembic import command
+    from alembic.config import Config
+
+    config = Config(alembic_cfg)
+    command.upgrade(config, "head")
 
 
 if __name__ == "__main__":
-    init()
+    test()
+    # deploy()
